38b8d9c96bbf5927ae4824c533a986111f06db8f
Joseph Arias <jaria001@odu.edu>
2023-02-02 21:14:32 -0500

    initialize postgresql database

--- communitycurator_backend/settings.py
+++ communitycurator_backend/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'communityApp',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'communitycurator_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'Ja521717',
+        'HOST': 'localhost',
+        'PORT': '5434'
     }
 }
 
