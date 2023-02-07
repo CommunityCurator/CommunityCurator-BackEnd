d850e907094bf26b05c772a62be0cc40908f6d1b
Joel Lavinder <jdlavinder@gmail.com>
2023-02-06 21:11:46 -0500

    Add user view

--- user/views.py
+++ user/views.py
@@ -18,3 +18,25 @@ def users(request):
             serializer.save()
             return Response({'user': serializer.data}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET','POST', 'DELETE'])
+def user(request, id):
+    try:
+        data = User.objects.get(pk=id)
+    except User.DoesNotExist:
+        raise Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = UserSerializer(data)
+        return Response({'user': serializer.data})
+
+    elif request.method == 'DELETE':
+        data.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+    elif request.method == 'POST':
+        serializer = UserSerializer(data, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'user': serializer.data})
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
