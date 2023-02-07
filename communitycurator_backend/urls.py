0edd0c700cfc6a43d07bad5528345009394ef96d
Joel Lavinder <jdlavinder@gmail.com>
2023-02-06 20:39:41 -0500

    user model url & settings update

--- communitycurator_backend/urls.py
+++ communitycurator_backend/urls.py
@@ -20,5 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('groups.urls')),
     path('', include('category.urls')),
-    
+    path('', include('user.urls')),
+
 ]
