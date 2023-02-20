27ea857554af8139f769ac5ca5f0d8b29aa098f5
Mariia Ellis <mvoro001@odu.edu>
2023-02-19 21:30:00 -0500

    Updated group model. Added state, image and createdAt fields

--- groups/models.py
+++ groups/models.py
@@ -5,3 +5,6 @@ class Group(models.Model):
     groupName = models.CharField(max_length=50)
     description = models.CharField(max_length=500)
     city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50, blank=True)
+    image = models.CharField(max_length=100, blank=True)
+    createdAt = models.DateField(blank=True)
