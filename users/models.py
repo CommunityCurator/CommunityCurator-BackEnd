5fe27dd52eebde9a58430b91abdd64e0bab6ab86
Amanda Macrorie <amacr001@odu.edu>
2023-02-26 12:41:39 -0500

    Auto grab datetime for comment,group,user models

--- users/models.py
+++ users/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    user_name = models.CharField(max_length=30)
+    user_name = models.CharField(max_length=30, unique=True)
     first_name = models.CharField(max_length=35)
     last_name = models.CharField(max_length=35)
-    email = models.EmailField()
+    email = models.EmailField(max_length=60, unique=True)
     password = models.CharField(max_length=20)
-    bio = models.CharField(max_length=500)
-    image = models.CharField(max_length=100)
-    created_at = models.DateTimeField()
-    groups = models.ManyToManyField('group.Group')
+    bio = models.CharField(max_length=500, blank=True)
+    image = models.CharField(max_length=100, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    groups = models.ManyToManyField('group.Group', blank=True)
 
