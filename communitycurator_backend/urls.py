bb48c7033851230c92411a86d4b5ca35a56ec81c
Joel Lavinder <jdlavinder@gmail.com>
2023-02-26 19:14:45 -0500

    Login Functionality

--- communitycurator_backend/urls.py
+++ communitycurator_backend/urls.py
@@ -18,6 +18,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path('api/', include(('core.routers', 'core'), namespace='core-api')),
     path('', include('group.urls')),
     path('', include('category.urls')),
     path('', include('comment.urls')),
