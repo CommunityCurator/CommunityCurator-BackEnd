9c51ee34dd5fbae6cc8b9df97b9c381a9b5f157f
TonyGrif <TonyGriffin2000@gmail.com>
2023-03-26 18:36:42 -0400

    starter category test

--- category/tests.py
+++ category/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+import pytest
+
+from .models import Category
 
 # Create your tests here.
+class TestCategory:
+    def test_name(self):
+        # Initial Test
+        newCategory = Category
+        newCategory.name = "Computer Science Majors"
+        assert newCategory.name == "Computer Science Majors"
+        
+        # Test character limit
+        
+        # Test already availble
