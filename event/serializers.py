aa72bd680007c47785ae8c7d48c6e11b596c1b8a
Amanda Macrorie <amacr001@odu.edu>
2023-03-28 15:43:20 -0400

    Update event variables and urls

--- event/serializers.py
+++ event/serializers.py
@@ -5,5 +5,5 @@ from .models import Event
 class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
-        fields = ['event_name', 'id', 'event_date', 'event_time', 'event_location', 'event_description', 'event_image', 'created_at', 'event_creator',] #'event_groups',
+        fields = ['event_name', 'id', 'event_date', 'event_time', 'event_city', 'event_state', 'event_description', 'event_categories', 'event_image', 'created_at', 'event_creator',] #'event_groups',
         depth = 2
--- event/urls.py
+++ event/urls.py
@@ -4,5 +4,4 @@ from . import views
 urlpatterns = [
     path('api/events/', views.event, name='events'),
     path('api/event/<int:id>', views.event, name='event'),
-    path('api/signup/', views.signup, name='signup'),
 ]
