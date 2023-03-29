9acb30158034b7dec18f6c3686b739beab1d50b2
Amanda Macrorie <amacr001@odu.edu>
2023-03-29 14:03:09 -0400

    Deleted event model, switching to ticketmaster api

--- event/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from .models import Event
-
-class EventSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Event
-        fields = ['event_name', 'id', 'event_date', 'event_time', 'event_city', 'event_state', 'event_description', 'event_categories', 'event_image', 'created_at', 'event_creator',] #'event_groups',
-        depth = 2
--- event/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('api/events/', views.event, name='events'),
-    path('api/event/<int:id>', views.event, name='event'),
-]
