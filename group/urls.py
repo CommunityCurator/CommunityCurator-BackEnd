5da47762a025280c86a16c557162721c7fcabba6
Amanda Macrorie <amacr001@odu.edu>
2023-03-30 13:31:39 -0400

    Added urls and rest calls for category filtering

--- group/urls.py
+++ group/urls.py
@@ -6,4 +6,6 @@ urlpatterns=[
     path ('api/group/<int:id>', views.group, name='group'),
     path ('api/groups/<city>', views.group_city, name='group_city'),
     path ('api/groups/<city>/<int:userid>', views.group_city_user, name='group_city_user'),
+    path ('api/groups/<category>', views.group_category, name='group_category'),
+    path ('api/groups/<category>/<int:userid>', views.group_category_user, name='group_category_user')
 ]
