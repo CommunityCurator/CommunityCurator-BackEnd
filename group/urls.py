976dd3e165885d210964b2e1e3c322f4ce4fbb4a
Amanda Macrorie <amacr001@odu.edu>
2023-04-01 17:45:25 -0400

    Implemented group search

--- group/urls.py
+++ group/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
 from . import views
+from .views import SearchGroupList
 
 urlpatterns=[
     path ('api/groups/', views.groups, name='groups'),
     path ('api/group/<int:id>', views.group, name='group'),
-    path ('api/groups/<city>', views.group_city, name='group_city'),
-    path ('api/groups/<city>/<int:userid>', views.group_city_user, name='group_city_user'),
-    path ('api/groups/<category>', views.group_category, name='group_category'),
-    path ('api/groups/<category>/<int:userid>', views.group_category_user, name='group_category_user')
+    path ('api/searchgroups/', SearchGroupList.as_view(), name='search_group_list'),
 ]
--- group/views.py
+++ group/views.py
@@ -1,6 +1,9 @@
 from group.models import Group
 from django.http import JsonResponse, Http404
+from django.db.models import Q
 from group.serializers import GroupSerializer
+from rest_framework import generics, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
@@ -40,47 +43,40 @@ def group(request, id):
             return Response({'group': serializer.data})
         return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
     
-@api_view(['GET'])
-def group_city(request, city):
-    try:
-        data = Group.objects.filter(city=city)
-    except Group.DoesNotExist:
-        raise Response(status=status.HTTP_404_NOT_FOUND)
+class SearchGroupList(generics.ListAPIView):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filterset_fields = ['city', 'categories', 'group_name']
+    ordering_fields = ['group_name']
 
-    if request.method == 'GET':
-        serializer = GroupSerializer(data, many=True)
-        return Response({'group_city': serializer.data})
-
-@api_view(['GET'])
-def group_city_user(request, city, userid):
-    try:
-        data = Group.objects.filter(city=city).exclude(users__id=userid)
-    except Group.DoesNotExist:
-        raise Response(status=status.HTTP_404_NOT_FOUND)
-
-    if request.method == 'GET':
-        serializer = GroupSerializer(data, many=True)
-        return Response({'group_city_user': serializer.data})
+    def get_queryset(self):
+        queryset = Group.objects.all()
+        city = self.request.query_params.get('city', None)
+        categories = self.request.query_params.get('categories', None)
+        group_name = self.request.query_params.get('group_name', None)
+        if city is not None:
+            queryset = queryset.filter(city=city)
+        if categories is not None:
+            queryset = queryset.filter(categories=categories)
+        if group_name is not None:
+            queryset = queryset.filter(group_name__icontains=group_name)
+        return queryset
     
-@api_view(['GET'])
-def group_category(request, category):
-    try:
-        data = Group.objects.filter(category=category)
-    except Group.DoesNotExist:
-        raise Response(status=status.HTTP_404_NOT_FOUND)
-
-    if request.method == 'GET':
-        serializer = GroupSerializer(data, many=True)
-        return Response({'group_category': serializer.data})
-    
-@api_view(['GET'])
-def group_category_user(request, category, userid):
-    try:
-        data = Group.objects.filter(category=category).exclude(users__id=userid)
-    except Group.DoesNotExist:
-        raise Response(status=status.HTTP_404_NOT_FOUND)
-
-    if request.method == 'GET':
-        serializer = GroupSerializer(data, many=True)
-        return Response({'group_category_user': serializer.data})
-    
+    #Beginning of a suggestion rest call, not yet fully implemented
+class SuggestGroupList(generics.ListAPIView):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filterset_fields = ['city', 'categories', 'joined_users', 'joined_users.categories']
+    ordering_fields = ['group_name']
+    def get_queryset(self):
+        queryset = Group.objects.all()
+        city = self.request.query_params.get('city', None)
+        categories = self.request.query_params.get('categories', None)
+        group_name = self.request.query_params.get('group_name', None)
+        if city is not None:
+            queryset = queryset.filter(city=city)
+        if categories is not None:
+            queryset = queryset.filter(categories=categories)
+        return queryset
