c37bebf1341adfd69724bcb968c7287018c6ba27
rmackley <rmack001@odu.edu>
2023-04-23 08:07:34 -0400

    Added api calls to pull counts for likes and dislikes

--- feedback/serializers.py
+++ feedback/serializers.py
@@ -2,7 +2,25 @@ from rest_framework import serializers
 from .models import Feedback
 
 class FeedbackSerializer(serializers.ModelSerializer):
+    
     class Meta:
         model = Feedback
-        fields = ['id', 'group', 'user', 'like', 'dislike', 'details', 'created_at']
-        depth = 3
+        fields = ['id', 'group', 'user', 'like', 'dislike', 'report', 'details', 'created_at']
+        depth = 2
+
+class LikeSerializer(serializers.ModelSerializer):
+    count_like = serializers.IntegerField(read_only=True) 
+    
+    class Meta:
+        model = Feedback
+        fields = ['id', 'count_like']
+
+class DislikeSerializer(serializers.ModelSerializer):
+    count_dislike = serializers.IntegerField(read_only=True) 
+    
+    class Meta:
+        model = Feedback
+        fields = ['id', 'count_dislike']
+        
+
+
--- feedback/views.py
+++ feedback/views.py
@@ -1,6 +1,6 @@
 from feedback.models import Feedback
 from django.http import JsonResponse, Http404
-from feedback.serializers import FeedbackSerializer
+from feedback.serializers import FeedbackSerializer, LikeSerializer, DislikeSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
@@ -81,12 +81,25 @@ def new_dislike(request):
         return JsonResponse(response, safe=False, status=201)
 
 @api_view(['GET'])
-def group_feedback_count(request, id):
+def group_like_count(request, id):
     try:
-        data = Feedback.objects.filter(group__id=id).annotate(likes=Count('like'))
+        data = Feedback.objects.filter(group__id=id, like="True") 
+        data = data.annotate(count_like=Count('id'))
     except Feedback.DoesNotExist:
         raise Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
-        serializer = FeedbackSerializer(data, many=True)
-        return Response({'feedbacks': serializer.data})
+        serializer = LikeSerializer(data, many=True)
+        return Response({'likes': data.count()})
+
+@api_view(['GET'])
+def group_dislike_count(request, id):
+    try:
+        data = Feedback.objects.filter(group__id=id, dislike="True") 
+        data = data.annotate(count_dislike=Count('id'))
+    except Feedback.DoesNotExist:
+        raise Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = DislikeSerializer(data, many=True)
+        return Response({'dislikes': data.count()})
